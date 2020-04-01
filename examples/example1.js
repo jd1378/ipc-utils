33f6c1f6fb3392c93c17dab367e63410ebb8efb4
Javad Mnjd <javad.mnjd@hotmail.com>
2020-04-01 17:58:36 +0430

    don't log exit code of child process

--- examples/example1.js
+++ examples/example1.js
@@ -6,7 +6,6 @@ async function useChildMethod() {
   // eslint-disable-next-line no-console
   console.log('child result: ', await parent.child.childMethod());
   await parent.removeComlink();
-  setTimeout(() => console.log(parent.childProcess.exitCode), 100);
 }
 
 useChildMethod();
